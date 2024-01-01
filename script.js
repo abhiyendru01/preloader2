146b582de2e7c94dabdbeeec2d6b5a518054b2e6
Rahul Raj <rahul.r0644@gmail.com>
2024-01-02 04:41:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.addEventListener('load', function () {
+  const preloader = document.querySelector('.preloader');
+  const content = document.querySelector('.content');
+
+  // Hide preloader and display content when the page is fully loaded
+  setTimeout(function() {
+    preloader.style.display = 'none';
+    content.style.display = 'block';
+  }, 2000); // You can adjust the delay time as needed
+});
 
